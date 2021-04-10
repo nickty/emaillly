6e03050f078f7405e1a5eaca839dd52ade4c15dd
Mizanur Rahman <nickty.86@gmail.com>
2021-04-10 19:47:30 +0600

    google authetication added

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 
 
 
-app.get('/', (req, res) => {
-    res.send({'mizan': "how are you you"})
-})
+passport.use(new GoogleStrategy({
+    clientID: process.env.Client_ID,
+    clientSecret: process.env.Client_Secret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log(accessToken)
+    console.log(refreshToken)
+    console.log(profile)
+}))
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
+
+
+app.get('/auth/google/callback', passport.authenticate('google'))
+
+
+
+// app.get('/', (req, res) => {
+//     res.send({'mizan': "how are you you"})
+// })
 
 
 const PORT = process.env.PORT || 5000
