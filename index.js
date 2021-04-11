c834ff04c7f724cc8942444cd8475f76659ce33e
Mizanur Rahman <nickty.86@gmail.com>
2021-04-11 20:02:23 +0600

    logout

--- index.js
+++ index.js
@@ -68,6 +68,10 @@ app.get('/api/curuser', (req, res) => {
     res.send(req.user)
 })
 
+app.get('/api/logout', (req, res) => {
+    req.logout()
+    res.send(req.user)
+})
 
 app.get('/auth/google/callback', passport.authenticate('google'))
 
